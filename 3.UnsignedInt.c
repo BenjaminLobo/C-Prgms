bc7730c7368a5c45d2c18c822547567e30f3d4f1
Benjamin lobo <89774926+BenjaminLobo@users.noreply.github.com>
2021-09-22 18:12:38 +0530

    Add files via upload

---
+++ 3.UnsignedInt.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    unsigned int distance;
+    printf("Enter the distance between two planets:");
+    scanf("%u",&distance);
+    printf("You have entered : %u",distance);
+    getch();
+}
