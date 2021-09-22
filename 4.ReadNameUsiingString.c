312fde0469253edc9dddbf35004b8d84b93c1cad
Benjamin lobo <89774926+BenjaminLobo@users.noreply.github.com>
2021-09-22 18:45:24 +0530

    Add files via upload

---
+++ 4.ReadNameUsiingString.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char name[30];
+    printf("Enter your name:");
+    scanf("%s",&name);
+    printf("Hi %s, Welcome to C-Programming",name);
+    getch();
+}
